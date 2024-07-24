a6c47dfe200d62fd806519ade0c0cf9e5c2140ea
mdfawazj <110121854+mdfawazj@users.noreply.github.com>
2024-07-23 22:51:33 -0500

    Update finalpullscript.py

--- finalpullscript.py
+++ finalpullscript.py
@@ -399,332 +399,124 @@ with open('all_resources.csv', 'w', newline='') as csvfile:
 ###############################################new add
 
 
-import boto3
-from botocore.exceptions import ClientError
 
-def list_aws_resources():
-    regions = boto3.session.Session().get_available_regions('ec2')
-    account_number = boto3.client('sts').get_caller_identity().get('Account')
 
-    resources = []
-    processed_s3_buckets = set()
 
-    for region in regions:
-        session = boto3.Session(region_name=region, verify=False)
-        clients = {
-            'ec2': session.client('ec2'),
-            'rds': session.client('rds'),
-            'lambda': session.client('lambda'),
-            'elb': session.client('elb'),
-            'elbv2': session.client('elbv2'),
-            'ecs': session.client('ecs'),
-            'dynamodb': session.client('dynamodb'),
-            'sns': session.client('sns'),
-            'sqs': session.client('sqs'),
-            'redshift': session.client('redshift'),
-            'dms': session.client('dms'),
-            'secretsmanager': session.client('secretsmanager'),
-            'elasticache': session.client('elasticache'),
-            'es': session.client('es'),
-            'elasticbeanstalk': session.client('elasticbeanstalk'),
-            's3': session.client('s3'),
-            'ecr': session.client('ecr'),
-            'route53': session.client('route53'),
-            'amplify': session.client('amplify'),
-            'cognito-idp': session.client('cognito-idp')
+To integrate AWS Cognito and Amplify into your existing script, you'll need to add code for fetching resources and their tags from these services. Here’s how you can extend your script to include AWS Cognito and Amplify:
+
+1. Import the Required Libraries
+Add cognito-idp and amplify to your create_clients function.
+
+python
+Copy code
+def create_clients(region):
+    return {
+        'ec2': boto3.client('ec2', region_name=region, verify=False),
+        'rds': boto3.client('rds', region_name=region, verify=False),
+        'lambda': boto3.client('lambda', region_name=region, verify=False),
+        'elb': boto3.client('elb', region_name=region, verify=False),
+        'elbv2': boto3.client('elbv2', region_name=region, verify=False),
+        'ecs': boto3.client('ecs', region_name=region, verify=False),
+        'dynamodb': boto3.client('dynamodb', region_name=region, verify=False),
+        'sns': boto3.client('sns', region_name=region, verify=False),
+        'sqs': boto3.client('sqs', region_name=region, verify=False),
+        'redshift': boto3.client('redshift', region_name=region, verify=False),
+        'dms': boto3.client('dms', region_name=region, verify=False),
+        'secretsmanager': boto3.client('secretsmanager', region_name=region, verify=False),
+        'elasticache': boto3.client('elasticache', region_name=region, verify=False),
+        'es': boto3.client('es', region_name=region, verify=False),
+        'elasticbeanstalk': boto3.client('elasticbeanstalk', region_name=region, verify=False),
+        's3': boto3.client('s3', region_name=region, verify=False),
+        'ecr': boto3.client('ecr', region_name=region, verify=False),
+        'route53': boto3.client('route53', region_name=region, verify=False),
+        'cognito-idp': boto3.client('cognito-idp', region_name=region, verify=False),
+        'amplify': boto3.client('amplify', region_name=region, verify=False),
+    }
+2. Add Functions to Fetch Cognito and Amplify Resources
+Here are the functions to fetch the Cognito User Pools, User Pool Clients, and Amplify Apps.
+
+python
+Copy code
+def get_cognito_user_pools(client):
+    user_pools = paginate_boto3_results(client, 'list_user_pools', 'UserPools')
+    for pool in user_pools:
+        tags = get_tags(client, 'cognito', pool['Id'])
+        yield {
+            'ResourceType': 'Cognito User Pool',
+            'ResourceArn': f"arn:aws:cognito-idp:{region}:{account_number}:userpool/{pool['Id']}",
+            'ResourceName': pool['Name'],
+            'Region': region,
+            'Tags': tags
         }
 
-        # Fetch EC2 instances
-        ec2_instances = paginate_boto3_results(clients['ec2'], 'describe_instances', 'Reservations')
-        for reservation in ec2_instances:
-            for instance in reservation['Instances']:
-                tags = get_tags(clients['ec2'], 'ec2', instance['InstanceId'])
-                resources.append({
-                    'ResourceType': 'EC2 Instance',
-                    'ResourceArn': instance['InstanceId'],
-                    'ResourceName': instance['InstanceId'],
-                    'Region': region,
-                    'Tags': tags
-                })
+def get_cognito_user_pool_clients(client, user_pool_id):
+    clients = paginate_boto3_results(client, 'list_user_pool_clients', 'UserPoolClients')
+    for client_data in clients:
+        tags = get_tags(client, 'cognito', client_data['ClientId'])
+        yield {
+            'ResourceType': 'Cognito User Pool Client',
+            'ResourceArn': f"arn:aws:cognito-idp:{region}:{account_number}:userpool/{user_pool_id}/client/{client_data['ClientId']}",
+            'ResourceName': client_data['ClientName'],
+            'Region': region,
+            'Tags': tags
+        }
 
-        # Fetch RDS instances
-        rds_instances = paginate_boto3_results(clients['rds'], 'describe_db_instances', 'DBInstances')
-        for instance in rds_instances:
-            tags = get_tags(clients['rds'], 'rds', instance['DBInstanceArn'])
-            resources.append({
-                'ResourceType': 'RDS Instance',
-                'ResourceArn': instance['DBInstanceArn'],
-                'ResourceName': instance['DBInstanceIdentifier'],
-                'Region': region,
-                'Tags': tags
-            })
+def get_amplify_apps(client):
+    apps = paginate_boto3_results(client, 'list_apps', 'apps')
+    for app in apps:
+        tags = get_tags(client, 'amplify', app['appId'])
+        yield {
+            'ResourceType': 'Amplify App',
+            'ResourceArn': f"arn:aws:amplify:{region}:{account_number}:apps/{app['appId']}",
+            'ResourceName': app['name'],
+            'Region': region,
+            'Tags': tags
+        }
+3. Integrate Cognito and Amplify Fetching into the Main Script
+Add the Cognito and Amplify resource fetching to your main loop.
 
-        # Fetch Lambda functions
-        lambda_functions = paginate_boto3_results(clients['lambda'], 'list_functions', 'Functions')
-        for function in lambda_functions:
-            tags = get_tags(clients['lambda'], 'lambda', function['FunctionArn'])
-            resources.append({
-                'ResourceType': 'Lambda Function',
-                'ResourceArn': function['FunctionArn'],
-                'ResourceName': function['FunctionName'],
-                'Region': region,
-                'Tags': tags
-            })
+python
+Copy code
+for region in regions:
+    print(f"Processing region: {region}")
+    clients = create_clients(region)
 
-        # Fetch Classic Load Balancers
-        elbs = paginate_boto3_results(clients['elb'], 'describe_load_balancers', 'LoadBalancerDescriptions')
-        for elb in elbs:
-            tags = get_tags(clients['elb'], 'elb', elb['LoadBalancerName'])
-            resources.append({
-                'ResourceType': 'Classic Load Balancer',
-                'ResourceArn': f"arn:aws:elb:{region}:{account_number}:loadbalancer/{elb['LoadBalancerName']}",
-                'ResourceName': elb['LoadBalancerName'],
-                'Region': region,
-                'Tags': tags
-            })
+    # Existing resource fetching...
 
-        # Fetch Application and Network Load Balancers
-        elbv2s = paginate_boto3_results(clients['elbv2'], 'describe_load_balancers', 'LoadBalancers')
-        for elbv2 in elbv2s:
-            tags = get_tags(clients['elbv2'], 'elbv2', elbv2['LoadBalancerArn'])
-            resources.append({
-                'ResourceType': 'Application/Network Load Balancer',
-                'ResourceArn': elbv2['LoadBalancerArn'],
-                'ResourceName': elbv2['LoadBalancerName'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch ECS clusters
-        ecs_clusters = paginate_boto3_results(clients['ecs'], 'list_clusters', 'clusterArns')
-        for cluster_arn in ecs_clusters:
-            cluster = clients['ecs'].describe_clusters(clusters=[cluster_arn])['clusters'][0]
-            tags = get_tags(clients['ecs'], 'ecs', cluster_arn)
-            resources.append({
-                'ResourceType': 'ECS Cluster',
-                'ResourceArn': cluster_arn,
-                'ResourceName': cluster['clusterName'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch DynamoDB tables
-        dynamodb_tables = paginate_boto3_results(clients['dynamodb'], 'list_tables', 'TableNames')
-        for table_name in dynamodb_tables:
-            table = clients['dynamodb'].describe_table(TableName=table_name)['Table']
-            tags = get_tags(clients['dynamodb'], 'dynamodb', table['TableArn'])
-            resources.append({
-                'ResourceType': 'DynamoDB Table',
-                'ResourceArn': table['TableArn'],
-                'ResourceName': table_name,
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch SNS topics
-        sns_topics = paginate_boto3_results(clients['sns'], 'list_topics', 'Topics')
-        for topic in sns_topics:
-            tags = get_tags(clients['sns'], 'sns', topic['TopicArn'])
-            resources.append({
-                'ResourceType': 'SNS Topic',
-                'ResourceArn': topic['TopicArn'],
-                'ResourceName': topic['TopicArn'].split(':')[-1],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch SQS queues
-        sqs_queues = paginate_boto3_results(clients['sqs'], 'list_queues', 'QueueUrls')
-        for queue_url in sqs_queues:
-            tags = get_tags(clients['sqs'], 'sqs', queue_url)
-            resources.append({
-                'ResourceType': 'SQS Queue',
-                'ResourceArn': queue_url,
-                'ResourceName': queue_url.split('/')[-1],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch Redshift clusters
-        redshift_clusters = paginate_boto3_results(clients['redshift'], 'describe_clusters', 'Clusters')
-        for cluster in redshift_clusters:
-            tags = get_tags(clients['redshift'], 'redshift', cluster['ClusterNamespaceArn'])
-            resources.append({
-                'ResourceType': 'Redshift Cluster',
-                'ResourceArn': cluster['ClusterNamespaceArn'],
-                'ResourceName': cluster['ClusterIdentifier'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch DMS replication instances
-        dms_instances = paginate_boto3_results(clients['dms'], 'describe_replication_instances', 'ReplicationInstances')
-        for instance in dms_instances:
-            tags = get_tags(clients['dms'], 'dms', instance['ReplicationInstanceArn'])
-            resources.append({
-                'ResourceType': 'DMS Replication Instance',
-                'ResourceArn': instance['ReplicationInstanceArn'],
-                'ResourceName': instance['ReplicationInstanceIdentifier'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch Secrets Manager secrets
-        secrets = paginate_boto3_results(clients['secretsmanager'], 'list_secrets', 'SecretList')
-        for secret in secrets:
-            tags = get_tags(clients['secretsmanager'], 'secretsmanager', secret['ARN'])
-            resources.append({
-                'ResourceType': 'Secrets Manager Secret',
-                'ResourceArn': secret['ARN'],
-                'ResourceName': secret['Name'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch ElastiCache clusters
-        elasticache_clusters = paginate_boto3_results(clients['elasticache'], 'describe_cache_clusters', 'CacheClusters')
-        for cluster in elasticache_clusters:
-            tags = get_tags(clients['elasticache'], 'elasticache', cluster['ARN'])
-            resources.append({
-                'ResourceType': 'ElastiCache Cluster',
-                'ResourceArn': cluster['ARN'],
-                'ResourceName': cluster['CacheClusterId'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch Elasticsearch domains
-        es_domains = paginate_boto3_results(clients['es'], 'list_domain_names', 'DomainNames')
-        for domain in es_domains:
-            domain_info = clients['es'].describe_elasticsearch_domain(DomainName=domain['DomainName'])
-            tags = get_tags(clients['es'], 'es', domain_info['DomainStatus']['ARN'])
-            resources.append({
-                'ResourceType': 'Elasticsearch Domain',
-                'ResourceArn': domain_info['DomainStatus']['ARN'],
-                'ResourceName': domain['DomainName'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch Elastic Beanstalk applications
-        elasticbeanstalk_apps = paginate_boto3_results(clients['elasticbeanstalk'], 'describe_applications', 'Applications')
-        for app in elasticbeanstalk_apps:
-            tags = get_tags(clients['elasticbeanstalk'], 'elasticbeanstalk', app['ApplicationArn'])
-            resources.append({
-                'ResourceType': 'Elastic Beanstalk Application',
-                'ResourceArn': app['ApplicationArn'],
-                'ResourceName': app['ApplicationName'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch S3 buckets
-        s3_buckets = clients['s3'].list_buckets()['Buckets']
-        for bucket in s3_buckets:
-            if bucket['Name'] not in processed_s3_buckets:
-                processed_s3_buckets.add(bucket['Name'])
-                tags = get_tags(clients['s3'], 's3', bucket['Name'])
-                resources.append({
-                    'ResourceType': 'S3 Bucket',
-                    'ResourceArn': f"arn:aws:s3:::{bucket['Name']}",
-                    'ResourceName': bucket['Name'],
-                    'Region': region,
-                    'Tags': tags
-                })
-
-        # Fetch ECR repositories
-        ecr_repos = paginate_boto3_results(clients['ecr'], 'describe_repositories', 'repositories')
-        for repo in ecr_repos:
-            tags = get_tags(clients['ecr'], 'ecr', repo['repositoryArn'])
-            resources.append({
-                'ResourceType': 'ECR Repository',
-                'ResourceArn': repo['repositoryArn'],
-                'ResourceName': repo['repositoryName'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch Route 53 hosted zones
-        route53_zones = paginate_boto3_results(clients['route53'], 'list_hosted_zones', 'HostedZones')
-        for zone in route53_zones:
-            tags = get_tags(clients['route53'], 'route53', zone['Id'])
-            resources.append({
-                'ResourceType': 'Route 53 Hosted Zone',
-                'ResourceArn': zone['Id'],
-                'ResourceName': zone['Name'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch Amplify apps
-        amplify_apps = paginate_boto3_results(clients['amplify'], 'list_apps', 'apps')
-        for app in amplify_apps:
-            tags = get_tags(clients['amplify'], 'amplify', app['appArn'])
-            resources.append({
-                'ResourceType': 'Amplify App',
-                'ResourceArn': app['appArn'],
-                'ResourceName': app['name'],
-                'Region': region,
-                'Tags': tags
-            })
-
-        # Fetch Cognito User Pools
-        cognito_user_pools = paginate_boto3_results(clients['cognito-idp'], 'list_user_pools', 'UserPools', MaxResults=60)
-        for pool in cognito_user_pools:
-            tags = get_tags(clients['cognito-idp'], 'cognito-idp', pool['Id'])
-            resources.append({
-                'ResourceType': 'Cognito User Pool',
-                'ResourceArn': pool['Id'],
-                'ResourceName': pool['Name'],
-                'Region': region,
-                'Tags': tags
-            })
-
-    return resources
-
-# Function to get tags for a resource
-def get_tags(client, service, resource_arn):
+    # Fetch Cognito User Pools
     try:
-        if service in ['ec2', 'rds', 'lambda', 's3', 'ecr', 'dynamodb']:
-            response = client.list_tags_for_resource(ResourceName=resource_arn)
-        elif service == 'sqs':
-            response = client.list_queue_tags(QueueUrl=resource_arn)
-        elif service == 'route53':
-            response = client.list_tags_for_resource(ResourceType='hostedzone', ResourceId=resource_arn.split('/')[-1])
-        elif service == 'es':
-            response = client.list_tags(ARN=resource_arn)
-        else:
-            response = client.list_tags_for_resource(ResourceArn=resource_arn)
-
-        return response.get('Tags', [])
+        for user_pool in get_cognito_user_pools(clients['cognito-idp']):
+            resources.append(user_pool)
     except ClientError as e:
-        print(f"Error fetching tags for {resource_arn}: {e}")
-        return []
+        print(f"Error fetching Cognito User Pools: {e}")
 
-# Function to paginate through boto3 results
-def paginate_boto3_results(client, operation, result_key, **kwargs):
-    paginator = client.get_paginator(operation)
-    result = []
-    for page in paginator.paginate(**kwargs):
-        result.extend(page[result_key])
-    return result
+    # Fetch Cognito User Pool Clients
+    try:
+        user_pools = paginate_boto3_results(clients['cognito-idp'], 'list_user_pools', 'UserPools')
+        for pool in user_pools:
+            for user_pool_client in get_cognito_user_pool_clients(clients['cognito-idp'], pool['Id']):
+                resources.append(user_pool_client)
+    except ClientError as e:
+        print(f"Error fetching Cognito User Pool Clients: {e}")
 
-# Main execution
-if __name__ == "__main__":
-    resources = list_aws_resources()
-    for resource in resources:
-        print(resource)
+    # Fetch Amplify Apps
+    try:
+        for app in get_amplify_apps(clients['amplify']):
+            resources.append(app)
+    except ClientError as e:
+        print(f"Error fetching Amplify Apps: {e}")
+
+    # Existing resource fetching...
+4. Update CSV Headers
+Add Cognito and Amplify tags to your CSV headers if needed.
+
+python
+Copy code
+csv_headers = ['Resource Type', 'Resource ARN', 'Region', 'Resource Name', 'Account Number'] + fiserv_tag_keys
+This approach will seamlessly integrate AWS Cognito and Amplify into your existing script without disrupting its current functionality. Adjust the tag handling as needed if you have specific tags or key-value pairs for Cognito and Amplify resources.
 
 
 
 
 
 
-Traceback (most recent call last):
-  File "H:\pychram\pulled\newtest\vernewcopy.py", line 313, in <module>
-    resources = list_aws_resources()
-                ^^^^^^^^^^^^^^^^^^^^
-  File "H:\pychram\pulled\newtest\vernewcopy.py", line 12, in list_aws_resources
-    session = boto3.Session(region_name=region, verify=False)
-              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-TypeError: Session.__init__() got an unexpected keyword argument 'verify'
