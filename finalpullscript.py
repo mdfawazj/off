2404f5ed778e6392bf781173ac335f726ddd81dd
mdfawazj <110121854+mdfawazj@users.noreply.github.com>
2024-07-23 23:22:06 -0500

    Update finalpullscript.py

--- finalpullscript.py
+++ finalpullscript.py
@@ -520,3 +520,42 @@ This approach will seamlessly integrate AWS Cognito and Amplify into your existi
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+Traceback (most recent call last):
+  File "H:\pychram\pulled\newtest\vernewcopy.py", line 394, in <module>
+    for user_pool in get_cognito_user_pools(clients['cognito-idp']):
+  File "H:\pychram\pulled\newtest\vernewcopy.py", line 41, in get_cognito_user_pools
+    user_pools = paginate_boto3_results(client, 'list_user_pools', 'UserPools')
+                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "H:\pychram\pulled\newtest\vernewcopy.py", line 36, in paginate_boto3_results
+    for page in paginator.paginate():
+  File "C:\Users\F37YHCS\AppData\Roaming\Python\Python311\site-packages\botocore\paginate.py", line 269, in __iter__
+    response = self._make_request(current_kwargs)
+               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\F37YHCS\AppData\Roaming\Python\Python311\site-packages\botocore\paginate.py", line 357, in _make_request
+    return self._method(**current_kwargs)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\F37YHCS\AppData\Roaming\Python\Python311\site-packages\botocore\client.py", line 565, in _api_call
+    return self._make_api_call(operation_name, kwargs)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\F37YHCS\AppData\Roaming\Python\Python311\site-packages\botocore\client.py", line 974, in _make_api_call
+    request_dict = self._convert_to_request_dict(
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\F37YHCS\AppData\Roaming\Python\Python311\site-packages\botocore\client.py", line 1048, in _convert_to_request_dict
+    request_dict = self._serializer.serialize_to_request(
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "C:\Users\F37YHCS\AppData\Roaming\Python\Python311\site-packages\botocore\validate.py", line 381, in serialize_to_request
+    raise ParamValidationError(report=report.generate_report())
+botocore.exceptions.ParamValidationError: Parameter validation failed:
+Missing required parameter in input: "MaxResults"
+
