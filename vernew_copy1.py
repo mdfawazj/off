a42d0907d7507deae65715fd44c06b04404788eb
mdfawazj <110121854+mdfawazj@users.noreply.github.com>
2024-07-28 01:02:41 -0500

    Update vernew_copy1.py

--- vernew_copy1.py
+++ vernew_copy1.py
@@ -1,3 +1,22 @@
+def paginate_boto3_results(client, method, key, requires_max_results=False, **kwargs):
+    paginator = client.get_paginator(method)
+    if requires_max_results:
+        pagination_config = {'MaxResults': 50}
+        for page in paginator.paginate(PaginationConfig=pagination_config, **kwargs):
+            for item in page[key]:
+                yield item
+    else:
+        for page in paginator.paginate(**kwargs):
+            for item in page[key]:
+                yield item
+
+
+
+
+
+
+
+
 Traceback (most recent call last):
   File "C:\Users\f37yhcs\Desktop\pulled\giftdev\vernew_copy1.py", line 273, in <module>
     for user_pool in cognito_user_pools:
